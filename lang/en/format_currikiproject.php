e4f39534d28c35abfe1b93ca1fc3e99df59f6026
Ali Mehdi <ali@curriki.org>
2022-01-19 15:33:40 +0000

    first push

---
+++ lang/en/format_currikiproject.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component Curriki project course format.
+ *
+ * @package   format_currikiproject
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['addsections'] = 'Add playlist';
+$string['currentsection'] = 'This playlist';
+$string['editsection'] = 'Edit playlist';
+$string['editsectionname'] = 'Edit playlist name';
+$string['deletesection'] = 'Delete playlist';
+$string['newsectionname'] = 'New name for playlist {$a}';
+$string['sectionname'] = 'Playlist';
+$string['pluginname'] = 'Curriki project format';
+$string['section0name'] = 'General';
+$string['page-course-view-currikiproject'] = 'Any course main page in curriki project format';
+$string['page-course-view-currikiproject-x'] = 'Any course page in curriki project format';
+$string['hidefromothers'] = 'Hide playlist';
+$string['showfromothers'] = 'Show playlist';
+$string['privacy:metadata'] = 'The Currki project format plugin does not store any personal data.';
